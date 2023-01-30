d8b4c9bbb257de8005f5d63555ff7ec9386691c8
amir-wyvern <amirhosein_wyvern@yahoo.com>
2023-01-30 17:51:09 +0330

    add new models

--- api/db/models.py
+++ api/db/models.py
@@ -23,22 +23,27 @@ class DbUser(Base):
     phone_number = Column(VARCHAR(14), index=True, unique=True, nullable=False)
     referal_link = Column(VARCHAR(100), index=True, unique=True, nullable=False)
     
+    password = Column(String(200), nullable=False)
     email = Column(String(100), unique=True, nullable=True)
-    name = Column(string(100) ,nullable=False)
+    name = Column(String(100) ,nullable=False)
     lastname = Column(String(100), nullable=False)
     balance = Column(Float(15,6), nullable=False, default=0.0 ) # CheckConstraint('balance >= 0')
     photo = Column(VARCHAR(200), nullable=True, unique=True)
     number_of_invented = Column(Integer, default=0, nullable=False) # CheckConstraint('number_of_invented >= 0')
-    bonus_of_invented = Column(Integer, default=0, nullable=False) # CheckConstraint('bonus_of_invented >= 0')
+    bonus_of_invented = Column(Float(15,6), default=0.0, nullable=False) # CheckConstraint('bonus_of_invented >= 0.0')
     register_time = Column(DateTime, nullable=False)
     first_deposit_value = Column(Integer) #  CheckConstraint('first_deposit_value >= 0')
 
-    main = relationship("main")
+    # relTransferHistory = relationship("DbTransferHistory")
+    # relWithdrawHistory = relationship("DbWithdrawHistory")
+    # relDepositHistory = relationship("DbDepositHistory")
+    # relRequestDeposit = relationship("DdRequestDeposit")
 
 
 class DbMainAccounts(Base):
     __tablename__ = 'main_accounts'
 
+    index = Column(Integer, primary_key=True)
     deposit_address = Column(VARCHAR(42), nullable=False)
     withdraw_address = Column(VARCHAR(42), nullable=False)
     p_withdraw = Column(VARCHAR(42), nullable=False)
@@ -47,7 +52,8 @@ class DbMainAccounts(Base):
 class DbConfig(Base):
     __tablename__ = 'config'
 
-    withdraw_lock = Column(Boolean, nullable=False, default=False)
+    index = Column(Integer, primary_key=True)
+    withdraw_lock = Column(Boolean, nullable=False, default=False) 
     deposit_lock = Column(Boolean, nullable=False, default=False)
     transfer_lock = Column(Boolean, nullable=False, default=False)
     transfer_fee_percentage = Column(Float(9,6), nullable=False, default=0.001) # CheckConstraint('transfer_fee_percentage >= 0')
@@ -56,11 +62,11 @@ class DbConfig(Base):
     referal_bonus_percentage = Column(Float(9,6), nullable=False, default=0.1) # CheckConstraint(referal_bonus_percentage >= 0)
 
 
-class DbPassword(Base):
-    __tablename__ = 'password'
+# class DbPassword(Base):
+#     __tablename__ = 'password'
 
-    user_id = Column(Integer, ForeignKey('user.user_id'), primary_key=True, nullable=False, unique=True, index=True)
-    password = Column(VARCHAR(200), nullable=False)
+#     user_id = Column(Integer, ForeignKey('user.user_id'), primary_key=True, nullable=False, unique=True, index=True)
+#     password = Column(VARCHAR(200), nullable=False)
 
 
 class DbTransferHistory(Base):
@@ -74,37 +80,46 @@ class DbTransferHistory(Base):
     transfer_fee_value = Column(Float(15,6), nullable=False) # CheckConstraint('transfer_fee_value > 0')
     timestamp = Column(DateTime, nullable=False)
 
-
-class DbWithdrawHistory(Base):
-    __tablename__ = 'withdraw_history'
-
-    tx_hash = Column(VARCHAR(100), primary_key=True, unique=True, index=True)
-    user_id = Column(Integer, ForeignKey('user.user_id'), index=True, nullable=False)
-    destination_address = Column(VARCHAR(42), index=True, nullable=False)
-    value = Column(Float(15,6), nullable=False) # CheckConstraint('value > 0')
-    withdraw_fee_percentage = Column(Float(9,6), nullable=False) # CheckConstraint('transfer_fee_percentage > 0')
-    withdraw_fee_value = Column(Float(15,6), nullable=False) # CheckConstraint('withdraw_fee_value > 0')
-    timestamp = Column(DateTime, nullable=False)
+    # relUser = relationship("DbUser")
 
 
-class DbDepositHistory(Base):
-    __tablename__ = 'deposit_history'
 
-    tx_hash = Column(VARCHAR(100), primary_key=True, unique=True, index=True)
-    user_id = Column(Integer, ForeignKey('user.user_id'), index=True, nullable=False)
-    origin_address = Column(VARCHAR(42), index=True, nullable=False)
-    value = Column(Float(15,6), nullable=False) # CheckConstraint('value > 0')
-    timestamp = Column(DateTime, nullable=False)
+# class DbWithdrawHistory(Base):
+#     __tablename__ = 'withdraw_history'
+
+#     tx_hash = Column(VARCHAR(100), primary_key=True, unique=True, index=True)
+#     user_id = Column(Integer, ForeignKey('user.user_id'), index=True, nullable=False)
+#     destination_address = Column(VARCHAR(42), index=True, nullable=False)
+#     value = Column(Float(15,6), nullable=False) # CheckConstraint('value > 0')
+#     withdraw_fee_percentage = Column(Float(9,6), nullable=False) # CheckConstraint('transfer_fee_percentage > 0')
+#     withdraw_fee_value = Column(Float(15,6), nullable=False) # CheckConstraint('withdraw_fee_value > 0')
+#     timestamp = Column(DateTime, nullable=False)
+
+#     relUser = relationship("DbUser")
 
 
-class DdRequestDeposit(Base):
-    __tablename__ = 'request_deposit'
+# class DbDepositHistory(Base):
+#     __tablename__ = 'deposit_history'
 
-    request_id = Column(Integer, index=True, primary_key=True, autoincrement=True)
-    user_id = Column(Integer, ForeignKey('user.user_id'), index=True, nullable=False)
-    origin_address = Column(VARCHAR(42), index=True, nullable=False)
-    value = Column(Float(15,6), nullable=False) # CheckConstraint('value > 0')
-    timestamp = Column(DateTime, nullable=False)
+#     tx_hash = Column(VARCHAR(100), primary_key=True, unique=True, index=True)
+#     user_id = Column(Integer, ForeignKey('user.user_id'), index=True, nullable=False)
+#     origin_address = Column(VARCHAR(42), index=True, nullable=False)
+#     value = Column(Float(15,6), nullable=False) # CheckConstraint('value > 0')
+#     timestamp = Column(DateTime, nullable=False)
+
+#     relUser = relationship("DbUser")
+
+
+# class DdRequestDeposit(Base):
+#     __tablename__ = 'request_deposit'
+
+#     request_id = Column(Integer, index=True, primary_key=True, autoincrement=True)
+#     user_id = Column(Integer, ForeignKey('user.user_id'), index=True, nullable=False)
+#     origin_address = Column(VARCHAR(42), index=True, nullable=False)
+#     value = Column(Float(15,6), nullable=False) # CheckConstraint('value > 0')
+#     timestamp = Column(DateTime, nullable=False)
+
+#     relUser = relationship("DbUser")
 
 
 
