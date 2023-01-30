e33a5e36aaea2490cbf5ff442ddd36655faaa0a2
amir-wyvern <amirhosein_wyvern@yahoo.com>
2023-01-30 17:59:14 +0330

    this file helped for hashing string

---
+++ api/db/hash.py
@@ -0,0 +1,21 @@
+import hashlib
+
+class Hash:
+
+    salt = 'dd211c31dd2abf6875ce5bb570cc71e9523386757d255a4f5155d'
+
+    @staticmethod
+    def sha3_256(password):
+
+        dataBase_password = password + Hash.salt
+        hashed_pass = hashlib.sha3_256(dataBase_password.encode()).hexdigest()
+
+        return hashed_pass
+
+    @staticmethod
+    def verify(hashed_password, plain_password):
+
+        if hashed_password == Hash.sha3_256(plain_password):
+            return True
+        
+        return False
