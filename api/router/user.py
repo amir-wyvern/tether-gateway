6f7cf3984dc706584cff79fa41f62e0264e7c269
amir-wyvern <amirhosein_wyvern@yahoo.com>
2023-02-01 20:37:53 +0330

    multi changed

--- api/router/user.py
+++ api/router/user.py
@@ -13,11 +13,10 @@ from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from typing import Optional, List, Dict
 from schemas import (
-    BotUserBase,
     UserDisplay,
-    BotUserRegister,
-    BotUserUpdateProfile,
-    BotUpdatePassword,
+    UserRegister,
+    UserUpdateProfile,
+    UpdatePassword,
     HTTPError,
     BaseResponse
 )
@@ -26,58 +25,51 @@ from db import db_user
 from db.database import get_db
 from db.hash import Hash
 
-router_telegram = APIRouter(prefix='/bot/user', tags=['Telegram Users'])
+router = APIRouter(prefix='/user', tags=['User'])
 
-
-@router_telegram.get('/info/{tel_id}', response_model=UserDisplay, responses={404:{'model':HTTPError}})
-def bot_user_info(tel_id: int, db=Depends(get_db)):
-    data = db_user.get_user_by_telegram_id(tel_id, db)
+@router.get('/info/{user_id}', response_model=UserDisplay, responses={404:{'model':HTTPError}})
+def user_info(user_id: int, db=Depends(get_db)):
+    data = db_user.get_user(user_id, db)
     if data is None:
         raise HTTPException(status_code=404, detail={'internal_code':1004, 'message':'The user not found'})
 
     return data
 
 
-@router_telegram.post('/register', response_model=UserDisplay, responses={403:{'model':HTTPError}})
-def bot_user_register(user: BotUserRegister, db=Depends(get_db)):
+@router.post('/register', response_model=UserDisplay, responses={403:{'model':HTTPError}})
+def user_register(user: UserRegister, db=Depends(get_db)):
 
-    data = db_user.get_user_by_telegram_id(user.tel_id, db)
+    data = db_user.check_exist_user(['email', 'tel_id', 'phone_number'] ,user, db) 
 
-    if data is not None:
+    if data is not None and data.tel_id == user.tel_id:
         raise HTTPException(status_code=403, detail={'internal_code':1005, 'message':'The user already exists!'})
 
-    data = db_user.get_user_by_phone_number(user.phone_number, db)
-
-    if data is not None:
+    if data is not None and data.phone_number == user.phone_number:
         raise HTTPException(status_code=403, detail={'internal_code':1006, 'message':'The phone number already exists!'})
 
-    data = db_user.get_user_by_email(user.email, db)
-
-    if data is not None:
+    if data is not None and data.email == user.email:
         raise HTTPException(status_code=403, detail={'internal_code':1007, 'message':'The email already exists!'})
 
     return db_user.create_user(user, db)
 
 
-@router_telegram.post('/update/{tel_id}', response_model=BaseResponse, responses={404:{'model':HTTPError}} )
-def bot_update_user_profile(tel_id:int, user: BotUserUpdateProfile, db=Depends(get_db)):
+@router.post('/update/{user_id}', response_model=BaseResponse, responses={404:{'model':HTTPError}, 403:{'model':HTTPError}} )
+def update_user_profile(user_id:int, user: UserUpdateProfile, db=Depends(get_db)):
 
-    old_data = db_user.get_user_by_telegram_id(tel_id, db)
+    old_data = db_user.get_user(user_id, db)
 
     if old_data is None:
         raise HTTPException(status_code=404, detail={'internal_code':1004, 'message':'The user not found'})
 
-    data = db_user.get_user_by_phone_number(user.phone_number, db)
+    data = db_user.check_exist_user(['email', 'phone_number'], user, db) 
 
-    if data and data.user_id != old_data.user_id:
+    if data and user_id != data.user_id and data.phone_number == user.phone_number: 
         raise HTTPException(status_code=403, detail={'internal_code':1006, 'message':'The phone number already exists!'})
 
-    data = db_user.get_user_by_email(user.email, db)
-
-    if data and data.user_id != old_data.user_id:
+    if data and user_id != data.user_id and data.email == user.email:
         raise HTTPException(status_code=403, detail={'internal_code':1007, 'message':'The email already exists!'})
 
-    resp = db_user.update_user_profile(old_data.user_id, user, db)
+    resp = db_user.update_user_profile(user_id, user, db)
 
     if resp == True:
         return JSONResponse(status_code=200, content={'message':'The user profile updated'})
@@ -86,25 +78,29 @@ def bot_update_user_profile(tel_id:int, user: BotUserUpdateProfile, db=Depends(g
         raise HTTPException(status_code=403, detail={'internal_code':1002 ,'message':'It is not possible to edit the information'})
     
 
+@router.post('/pass/{user_id}', response_model=BaseResponse, responses={404:{'model':HTTPError}, 403:{'model':HTTPError}})
+def update_user_password(user_id:int, user: UpdatePassword, db=Depends(get_db)):
 
-@router_telegram.post('/pass/{tel_id}', response_model=BaseResponse, responses={404:{'model':HTTPError}, 403:{'model':HTTPError}})
-def bot_update_user_password(tel_id:int, user: BotUpdatePassword, db=Depends(get_db)):
-
-    data = db_user.get_user_by_telegram_id(tel_id, db)
+    data = db_user.get_user(tel_id, db)
     if data is None:
         raise HTTPException(status_code=404, detail={'internal_code':1004, 'message':'The user not found'})
 
     if Hash.verify(data.password, user.old_password):
 
-        db_user.update_password(data.user_id, user, db)
+        db_user.update_password(user_id, user, db)
         return JSONResponse(status_code=200, content={'message':'The user password updated'})
     
     else:
         raise HTTPException(status_code=403, detail={'internal_code':1001, 'message':'The password is wrong'})
 
 
+@router.post('/test/')
+def update_user_password(user: UserRegister, db=Depends(get_db)):
 
+    # attrs = {UserRegister.email:user.email, UserRegister.phone_number:user.phone_number}
+    x = db_user.check_exist_user(user, db)
+    print(x[0].email )
 # # delete user
-# @router_telegram.get('/delete/{tel_id}')
+# @router.get('/delete/{tel_id}')
 # def delete_user(tel_id:int, db=Depends(get_db)):
 #     return db_user.delete_user(tel_id, db)
