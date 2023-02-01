5bd0065b397e78e503f7ab656a121d8d1ca9ef5d
amir-wyvern <amirhosein_wyvern@yahoo.com>
2023-02-01 20:40:27 +0330

    new files

---
+++ api/celery_tasks/tasks.py
@@ -0,0 +1,60 @@
+import celery
+
+
+class WithdrawCeleryTask(celery.Task):
+    name = 'Withdraw_celery_task'
+
+    def run(self, payload):
+        """
+        place holder method
+        """
+        pass
+
+
+class DepositCeleryTask(celery.Task):
+    name = 'Deposit_celery_task'
+
+    def run(self, payload):
+        """
+        place holder method
+        """
+        pass
+
+
+class TransferCeleryTask(celery.Task):
+    name = 'Transfer_celery_task'
+
+    def run(self, payload):
+        """
+        place holder method
+        """
+        pass
+
+
+class ContractCeleryTask(celery.Task):
+    name = 'Contract_celery_task'
+
+    def run(self, payload):
+        """
+        place holder method
+        """
+        pass
+
+class NotificationCeleryTask(celery.Task):
+    name = 'notification_celery_task'
+
+    def run(self, payload):
+        """
+        place holder method
+        """
+        pass
+
+
+class DivisionCeleryTask(celery.Task):
+    name = 'division_celery_task'
+
+    def run(self, payload):
+        """
+        place holder method
+        """
+        pass
