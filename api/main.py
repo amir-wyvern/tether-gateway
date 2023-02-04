fde867376393fc1ce01178255220f3915795114e
amir-wyvern <amirhosein_wyvern@yahoo.com>
2023-02-05 03:00:15 +0330

    update file

--- api/main.py
+++ api/main.py
@@ -1,18 +1,18 @@
-from fastapi import FastAPI, status, Depends
-from fastapi.requests import Request
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI
 
 from router import user, deposit
-# from auth import authentication
 from db import models, db_config
 from db.database import engine, get_db
-from exceptions import EmailNotValid
+from dotenv import load_dotenv
+import os
+from pathlib import Path
 
+dotenv_path = Path('.env')
+load_dotenv(dotenv_path=dotenv_path)
 
-app = FastAPI()
+app = FastAPI() 
 app.include_router(user.router)
-# app.include_router(deposit.router)
-
+app.include_router(deposit.router)
 
 models.Base.metadata.create_all(engine)
-db_config.init_table(get_db().__next__())
+db_config.init_table(get_db().__next__()) 
