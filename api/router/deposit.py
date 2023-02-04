b7299d4892994ee10a0879d2dc24953911ef66e6
amir-wyvern <amirhosein_wyvern@yahoo.com>
2023-02-05 02:53:35 +0330

    update files

--- api/router/deposit.py
+++ api/router/deposit.py
@@ -1,82 +1,63 @@
-
+from fastapi.responses import JSONResponse
 from fastapi import (
     APIRouter,
-    Query,
-    Body,
-    Path,
     Depends,
-    status,
     HTTPException
 )
-
-from fastapi.responses import JSONResponse
-
-from pydantic import BaseModel
-from typing import Optional, List, Dict
 from schemas import (
-    UserBase,
-    UserDisplay,
-    UserRegister,
-    UserUpdateProfile,
-    UpdatePassword,
     HTTPError,
     BaseResponse,
-    RequestDeposit,
-    ConfirmationDeposit,
-    RequestDepositResponse
+    DepositRequest,
+    DepositConfirmation,
+    DepositRequestResponse,
+    DepositHistoryResponse,
+    DepositHistoryRequest
 )
 
-from db import db_request_deposit, db_user
+from db import db_deposit_request
 from db.database import get_db
-from db.hash import Hash
 
-from celery_tasks.tasks import DepositCeleryTask #, DivisionCeleryTask
+from celery_tasks.tasks import DepositCeleryTask 
 from celery_tasks.utils import create_worker_from
+from sqlalchemy.orm.session import Session
+from auth.oauth2 import ( 
+    get_current_user
+)
 
 
-router_telegram = APIRouter(prefix='/deposit', tags=['Telegram Users', 'Deposit'])
+router = APIRouter(prefix='/deposit', tags=['Deposit'])
 
 _, deposit_worker = create_worker_from(DepositCeleryTask)
 
+@router.post('/request', response_model=DepositRequestResponse, responses={403:{'model':HTTPError}})
+def deposit_request(request: DepositRequest, user_id: int=Depends(get_current_user), db: Session=Depends(get_db)):
 
-@router_telegram.post('/request/{tel_id}', response_model=RequestDepositResponse, responses={403:{'model':HTTPError}})
-def _request_deposit(tel_id: int, user: RequestDeposit, db=Depends(get_db)):
+    resp = db_deposit_request.create_request(user_id, request, db)
 
-    data = db_user.get_user_by_telegram_id(tel_id, db)
-    if data is None:
-        raise HTTPException(status_code=404, detail={'internal_code':1004, 'message':'The user not found'})
-
-    resp = db_request_deposit.create_request(user, db)
-    
-    if resp == True:
-        JSONResponse(status_code=200, content={'request_id': resp.request_id ,'message':'Deposit request registered'})
+    if resp:
+        return JSONResponse(status_code=200, content={'request_id': resp.request_id ,'message':'Deposit request registered'})
 
     else:
         raise HTTPException(status_code=403, detail={'internal_code':1003, 'message':'There was a problem in registering the deposit request'})
 
 
-@router_telegram.post('/confirmation/{tel_id}', response_model=BaseResponse, responses={404:{'model':HTTPError}})
-def _comfirmation_deposit(tel_id: int, user: ConfirmationDeposit, db=Depends(get_db)):
-
-    data = db_user.get_user_by_telegram_id(tel_id, db)
-    if data is None:
-        raise HTTPException(status_code=404, detail={'internal_code':1004, 'message':'The user not found'})
+@router.post('/confirmation', response_model=BaseResponse, responses={404:{'model':HTTPError}})
+def deposit_comfirmation(request: DepositConfirmation, user_id: int=Depends(get_current_user), db: Session=Depends(get_db)):
 
     # send request to celery >> 
 
     payload = {
-        'user_id': data.user_id,
-        'tx_hash': data.tx_hash
+        'user_id': user_id,
+        'tx_hash': request.tx_hash
     }
 
     deposit_worker.apply_async(args=(payload, )) 
 
+    return JSONResponse(status_code=200, content={'message':'the request is proccessing'})
 
 
-    # resp = db_request_deposit.create_request(user, db)
-    # if resp and resp.request_id:
-    #     JSONResponse(status_code=200, content={'message':'Deposit request registered'})
+@router.post('/history', response_model=DepositHistoryResponse, responses={404:{'model':HTTPError}})
+def deposit_comfirmation(request: DepositHistoryRequest, user_id: int=Depends(get_current_user), db: Session=Depends(get_db)):
 
-    # else:
-    #     raise HTTPException(status_code=403, detail={'internal_code':1003, 'message':'There was a problem in registering the deposit request'})
+    return JSONResponse(status_code=200, content={'txs':[] })
 
--- api/router/user.py
+++ api/router/user.py
@@ -1,43 +1,39 @@
+from fastapi.responses import JSONResponse
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
+from sqlalchemy.orm.session import Session
 from fastapi import (
     APIRouter,
-    Query,
-    Body,
-    Path,
     Depends,
-    status,
-    HTTPException
+    HTTPException,
 )
-
-from fastapi.responses import JSONResponse
-
-from pydantic import BaseModel
-from typing import Optional, List, Dict
 from schemas import (
     UserDisplay,
     UserRegister,
     UserUpdateProfile,
     UpdatePassword,
     HTTPError,
-    BaseResponse
+    BaseResponse,
+    UserAuthResponse
 )
-
 from db import db_user
 from db.database import get_db
 from db.hash import Hash
+from auth.oauth2 import ( 
+    get_current_user,
+    create_access_token
+)
 
 router = APIRouter(prefix='/user', tags=['User'])
 
-@router.get('/info/{user_id}', response_model=UserDisplay, responses={404:{'model':HTTPError}})
-def user_info(user_id: int, db=Depends(get_db)):
-    data = db_user.get_user(user_id, db)
-    if data is None:
-        raise HTTPException(status_code=404, detail={'internal_code':1004, 'message':'The user not found'})
 
-    return data
+@router.get('/info', response_model=UserDisplay, responses={404:{'model':HTTPError}})
+def user_info(user_id: int=Depends(get_current_user), db: Session=Depends(get_db)):
+    
+    return db_user.get_user(user_id, db)
 
 
-@router.post('/register', response_model=UserDisplay, responses={403:{'model':HTTPError}})
-def user_register(user: UserRegister, db=Depends(get_db)):
+@router.post('/register', response_model=UserAuthResponse, responses={403:{'model':HTTPError}})
+def user_register(user: UserRegister, db: Session=Depends(get_db)):
 
     data = db_user.check_exist_user(['email', 'tel_id', 'phone_number'] ,user, db) 
 
@@ -50,26 +46,65 @@ def user_register(user: UserRegister, db=Depends(get_db)):
     if data is not None and data.email == user.email:
         raise HTTPException(status_code=403, detail={'internal_code':1007, 'message':'The email already exists!'})
 
-    return db_user.create_user(user, db)
+    if user.referal_link is not None:
+
+        resp = db_user.get_user_by_referal_link(user.referal_link, db)
+
+        if resp is None:
+            raise HTTPException(status_code=403, detail={'internal_code':1008, 'message':'The referal link not exists!'})
+        
+        db_user.increase_number_of_invented(resp.user_id, db)
+
+    new_user_data = db_user.create_user(user, db)
+    
+    if new_user_data is None:
+        raise HTTPException(status_code=403, detail={'internal_code':1010, 'message':'There was a problem in registering!'})
+    
+    access_token = create_access_token(data={'sub': new_user_data.user_id})
+
+    resp = {
+        'access_token': access_token,
+        'type_token': 'bearer'
+    }
+
+    return UserAuthResponse(**resp)
 
 
-@router.post('/update/{user_id}', response_model=BaseResponse, responses={404:{'model':HTTPError}, 403:{'model':HTTPError}} )
-def update_user_profile(user_id:int, user: UserUpdateProfile, db=Depends(get_db)):
+@router.post('/login', response_model=UserAuthResponse, responses={401:{'model':HTTPError}})
+def user_login(request: OAuth2PasswordRequestForm=Depends(), db: Session=Depends(get_db)):
 
-    old_data = db_user.get_user(user_id, db)
+    number_phone = request.username
+    data = db_user.get_user_by_phone_number(number_phone, db)
 
-    if old_data is None:
-        raise HTTPException(status_code=404, detail={'internal_code':1004, 'message':'The user not found'})
+    print(number_phone, data.__dict__)
+    if data is None :
+        raise HTTPException(status_code=401, detail={'internal_code':1009, 'message':'The phone number or password is incorrect'})
 
-    data = db_user.check_exist_user(['email', 'phone_number'], user, db) 
+    if Hash.verify(data.password, request.password) == False:
+        raise HTTPException(status_code=401, detail={'internal_code':1009, 'message':'The phone number or password is incorrect'})
 
-    if data and user_id != data.user_id and data.phone_number == user.phone_number: 
+    access_token = create_access_token(data={'user_id': data.user_id})
+
+    resp = {
+        'access_token': access_token,
+        'type_token': 'bearer',
+    }
+
+    return UserAuthResponse(**resp)
+
+
+@router.post('/update', response_model=BaseResponse, responses={404:{'model':HTTPError}, 403:{'model':HTTPError}} )
+def update_user_profile(request: UserUpdateProfile, user_id: int=Depends(get_current_user), db: Session=Depends(get_db)):
+
+    data = db_user.check_exist_user(['email', 'phone_number'], request, db) 
+
+    if data and user_id != data.user_id and data.phone_number == request.phone_number: 
         raise HTTPException(status_code=403, detail={'internal_code':1006, 'message':'The phone number already exists!'})
 
-    if data and user_id != data.user_id and data.email == user.email:
+    if data and user_id != data.user_id and data.email == request.email:
         raise HTTPException(status_code=403, detail={'internal_code':1007, 'message':'The email already exists!'})
 
-    resp = db_user.update_user_profile(user_id, user, db)
+    resp = db_user.update_user_profile(user_id, request, db)
 
     if resp == True:
         return JSONResponse(status_code=200, content={'message':'The user profile updated'})
@@ -78,29 +113,14 @@ def update_user_profile(user_id:int, user: UserUpdateProfile, db=Depends(get_db)
         raise HTTPException(status_code=403, detail={'internal_code':1002 ,'message':'It is not possible to edit the information'})
     
 
-@router.post('/pass/{user_id}', response_model=BaseResponse, responses={404:{'model':HTTPError}, 403:{'model':HTTPError}})
-def update_user_password(user_id:int, user: UpdatePassword, db=Depends(get_db)):
+@router.post('/pass', response_model=BaseResponse, responses={404:{'model':HTTPError}, 403:{'model':HTTPError}})
+def update_user_password(request: UpdatePassword, user_id: int=Depends(get_current_user), db: Session=Depends(get_db)):
 
-    data = db_user.get_user(tel_id, db)
-    if data is None:
-        raise HTTPException(status_code=404, detail={'internal_code':1004, 'message':'The user not found'})
+    data = db_user.get_user(user_id, db)
+    if Hash.verify(data.password, request.old_password):
 
-    if Hash.verify(data.password, user.old_password):
-
-        db_user.update_password(user_id, user, db)
+        db_user.update_password(user_id, request, db)
         return JSONResponse(status_code=200, content={'message':'The user password updated'})
     
     else:
         raise HTTPException(status_code=403, detail={'internal_code':1001, 'message':'The password is wrong'})
-
-
-@router.post('/test/')
-def update_user_password(user: UserRegister, db=Depends(get_db)):
-
-    # attrs = {UserRegister.email:user.email, UserRegister.phone_number:user.phone_number}
-    x = db_user.check_exist_user(user, db)
-    print(x[0].email )
-# # delete user
-# @router.get('/delete/{tel_id}')
-# def delete_user(tel_id:int, db=Depends(get_db)):
-#     return db_user.delete_user(tel_id, db)
