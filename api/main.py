6d5fb3bb21a8d84096f2e0c9488ab5bc0d515e4e
amir-wyvern <amirhosein_wyvern@yahoo.com>
2023-02-05 06:28:50 +0330

    edit file

--- api/main.py
+++ api/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 
-from router import user, deposit
+from router import (
+    user,
+    deposit,
+    withdraw,
+    transfer
+)
 from db import models, db_config
 from db.database import engine, get_db
 from dotenv import load_dotenv
@@ -13,6 +18,8 @@ load_dotenv(dotenv_path=dotenv_path)
 app = FastAPI() 
 app.include_router(user.router)
 app.include_router(deposit.router)
+app.include_router(withdraw.router)
+app.include_router(transfer.router)
 
 models.Base.metadata.create_all(engine)
 db_config.init_table(get_db().__next__()) 
