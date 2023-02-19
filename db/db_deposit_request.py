0bc4bed921f6910d1908428f199d531d8a60b76a
amir-wyvern <amirhosein_wyvern@yahoo.com>
2023-02-19 17:36:59 +0330

    fix bugs and add multi query

--- db/db_deposit_request.py
+++ db/db_deposit_request.py
@@ -1,18 +1,16 @@
 from sqlalchemy.orm.session import Session
-from schemas import (
-    DepositRequest
-)
 from db.models import DdDepositRequest, DepositRequestStatus
 from datetime import datetime
+from typing import Union
+from sqlalchemy import or_
 
-
-def create_request(user_id ,request: DepositRequest, db: Session):
+def create_request(user_id ,value, destination_address, db: Session):
     
     request_deposit = DdDepositRequest(
         user_id= user_id,
-        origin_address= request.origin_address,
-        destination_address= request.destination_address,
-        value= request.value,
+        # origin_address= request.origin_address,
+        destination_address= destination_address,
+        value= value,
         status= DepositRequestStatus.WAITING,
         error_message=None,
         timestamp= datetime.now()
@@ -25,14 +23,27 @@ def create_request(user_id ,request: DepositRequest, db: Session):
     return request_deposit
 
 
-def get_request_deposit_by_id(request_id, db:Session):
+def get_request_deposit_by_id(request_id, db:Session, mode: Union[DepositRequestStatus, None]= None):
+
     return db.query(DdDepositRequest).filter(DdDepositRequest.request_id == request_id ).first()
 
 
-def get_request_deposit_by_address(address, db:Session):
-    return db.query(DdDepositRequest).filter(DdDepositRequest.origin_address == address ).all()
+def get_request_deposit_by_address(address, db:Session, mode: Union[DepositRequestStatus, None]= None):
+
+    if mode == None:
+        return db.query(DdDepositRequest).filter(DdDepositRequest.destination_address == address ).all()
+    
+    else:
+        return db.query(DdDepositRequest).filter(or_(DdDepositRequest.destination_address == address, DdDepositRequest.status == mode) ).all()
 
 
-def get_request_deposit_by_user(user_id, db:Session):
-    return db.query(DdDepositRequest).filter(DdDepositRequest.user_id == user_id).all()
+def get_request_deposit_by_user(user_id, db:Session, mode: Union[DepositRequestStatus, None]= None):
+
+    if mode == None:
+        return db.query(DdDepositRequest).filter(DdDepositRequest.user_id == user_id).all()
+    
+    else:
+        return db.query(DdDepositRequest).filter(or_(DdDepositRequest.user_id == user_id, DdDepositRequest.status == mode)).all()
+
+
 
