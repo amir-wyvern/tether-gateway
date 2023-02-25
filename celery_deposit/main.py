90ceefe9347e4e3589e4fb08ef9f450b0cbf9ab7
amir-wyvern <amirhosein_wyvern@yahoo.com>
2023-02-25 20:46:04 +0330

    add func get_tx_time and fix bugs

--- celery_deposit/main.py
+++ celery_deposit/main.py
@@ -28,6 +28,7 @@ from celery_tasks.utils import create_worker_from
 from schemas import DepositHistoryModelForDataBase
 
 from web3 import Web3
+from web3.middleware import geth_poa_middleware
 from web3.exceptions import  TransactionNotFound
 
 import json
@@ -35,7 +36,7 @@ from dotenv import dotenv_values
 from datetime import datetime, timedelta
 import logging
 
-# logging.basicConfig(level=logging.DEBUG, 
+# logger.basicConfig(level=logger.DEBUG, 
 #                     format='%(asctime)s - %(levelname)s | %(message)s')
 
 
@@ -51,7 +52,7 @@ logger.addHandler(console_handler)
 
 # Create a file handler to save logs to a file
 file_handler = logging.FileHandler('deposit_service.log')
-file_handler.setLevel(logging.INFO)
+file_handler.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(levelname)s | %(message)s')
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
@@ -73,9 +74,8 @@ def safe_financial(func):
         user_id = args[1]['user_id']
         db = get_redis_cache().__next__()
 
-        logging.debug(f'cache_session > get_status_lock_from_user > request  [user_id: {user_id}]')
         safe_lock = get_status_lock_from_user(user_id, db) 
-        logging.debug(f'cache_session > get_status_lock_from_user > response [user_id: {user_id} -result: {safe_lock}]')
+        logger.debug(f'cache_session > get_status_lock_from_user > response [user_id: {user_id} -result: {safe_lock}]')
 
         if safe_lock == 'True':
             #send again to celery queue
@@ -84,15 +84,17 @@ def safe_financial(func):
             deposit_worker.apply_async(args=(arg,), countdown=15)
             return
         
-        logging.debug(f'cache_session > set_lock_for_user > request  [user_id: {user_id}]')
         resp = set_lock_for_user(user_id, db)
-        logging.debug(f'cache_session > set_lock_for_user > response [user_id: {user_id} -result: {resp}]')
+        logger.debug(f'cache_session > set_lock_for_user > response [user_id: {user_id} -result: {resp}]')
 
-        func(*args, **kwargs)
+        try :
+            func(*args, **kwargs)
 
-        logging.debug(f'cache_session > unlock_user > request  [user_id: {user_id}]')
+        except Exception as e:
+            logger.error(f'Exception [user_id: {user_id} -exception: {e}]')
+
         resp = unlock_user(user_id, db)
-        logging.debug(f'cache_session > unlock_user > response [user_id: {user_id} -result: {resp}]')
+        logger.debug(f'cache_session > unlock_user > response [user_id: {user_id} -result: {resp}]')
 
     return wrapper
 
@@ -102,6 +104,7 @@ class Contract:
 
         logger.debug('Contract class is initialing...')
         self.w3 = Web3(Web3.HTTPProvider(config["PROVIDER_1"]))
+        self.w3.middleware_onion.inject(geth_poa_middleware, layer=0)
         abi = config["ABI"]
         abi = abi.replace('\'', '"')
         abi = json.loads(abi)
@@ -129,6 +132,31 @@ class Contract:
                 logger.debug(f'tx_receipt > Exception [tx_hash: {tx_hash} -exception: {e}]')
                 if failed_count_of_req >= 3:
                     logger.warning(f'tx_receipt > Exception > count > 3 [tx_hash: {tx_hash}]')
+                    return False 
+                
+                failed_count_of_req += 1
+
+    def get_tx_time(self, blockNumber) -> datetime:
+
+        logger.debug(f'call get_tx_time [{blockNumber}]')
+
+        failed_count_of_req = 0
+        while True:
+            
+            try:
+                timestamp = self.w3.eth.get_block(blockNumber).timestamp
+                logger.debug(f'get_tx_time > response [blockNumber: {blockNumber} -time: {timestamp}]')
+                return datetime.fromtimestamp(timestamp)
+
+            except TransactionNotFound as e:
+                #send notifaction is faild
+                logger.debug(f'get_tx_time > TransactionNotFound [blockNumber: {blockNumber}]')
+                return False
+
+            except Exception as e:
+                logger.debug(f'get_tx_time > Exception [blockNumber: {blockNumber} -exception: {e}]')
+                if failed_count_of_req >= 3:
+                    logger.warning(f'get_tx_time > Exception > count > 3 [blockNumber: {blockNumber} -exception: {e}]')
                     return False 
                 
                 failed_count_of_req += 1
@@ -162,8 +190,7 @@ class DepositCeleryTaskImpl(DepositCeleryTask):
             logger.info(f'deposit is locked [user_id: {user_id} -tx_hash: {tx_hash}]')
             return
             
-        logger.debug(f'db_deposit_request > get_request_deposit_by_user > request  [user_id: {user_id} -state: {DepositRequestStatus.PROCESSING}]')
-        no_check_deposit_requests = get_request_deposit_by_user(user_id, db, DepositRequestStatus.PROCESSING)
+        no_check_deposit_requests = get_request_deposit_by_user(user_id, db, DepositRequestStatus.WAITING)
         logger.debug(f'db_deposit_request > get_request_deposit_by_user > response [user_id: {user_id} -result:{len(no_check_deposit_requests) * "*"}')
 
         deposit_requests = []
@@ -176,7 +203,6 @@ class DepositCeleryTaskImpl(DepositCeleryTask):
             # send to notifaction
             return
         
-        logger.debug(f'db_deposit_request > get_history_deposit_by_tx_hash > request  [user_id: {user_id} -tx_hash: {tx_hash}')
         old_tx_hash = get_history_deposit_by_tx_hash(tx_hash, db)
         logger.debug(f'db_deposit_request > get_history_deposit_by_tx_hash > response [user_id: {user_id} -tx_hash: {tx_hash} -result: {old_tx_hash is not None}')
         
@@ -197,7 +223,12 @@ class DepositCeleryTaskImpl(DepositCeleryTask):
             # send to notifaction
             return
         
+        timestamp = self.contract.get_tx_time(receipt['blockNumber'])
         
+        if datetime.now() - timestamp > timedelta(days=1) :
+            logger.info(f'A long time has passed since the transaction and it is not accepted [user_id: {user_id} -tx_hash: {tx_hash} -timedelta: {datetime.now() - timestamp }]')
+            return
+
         state = False
         for log in receipt['logs']:
 
@@ -207,7 +238,7 @@ class DepositCeleryTaskImpl(DepositCeleryTask):
                 
                 from_address=  Contract.to_check_sum_address(log.topics[1].hex())
                 to_address = Contract.to_check_sum_address(log.topics[2].hex())
-                if to_address != Contract.to_check_sum_address( get_deposit_address(db) ):
+                if to_address != Contract.to_check_sum_address( get_deposit_address(db)):
                     
                     logger.info(f'The to_address of transaction is not match with to deposit address [user_id: {user_id} -tx_hash: {tx_hash}] ')
                     break
@@ -234,20 +265,21 @@ class DepositCeleryTaskImpl(DepositCeleryTask):
                     'user_id': user_id,
                     'from_address': from_address ,
                     'to_address': to_address,
-                    'error_message': '',
+                    'error_message': None,
                     'status': DepositHistoryStatus.SUCCESS,
                     'value': value,
                     'timestamp': datetime.now()
                 }
-                
-                logger.debug(f'Saving a transaction in deposit history DB [data: {data}]')
 
                 try:
+                    
                     increase_balance(request.user_id, value, db, commit=False)
                     create_deposit_history(DepositHistoryModelForDataBase(**data), db, commit=False)
-                    update_status_by_request_id(request.request_id, DepositHistoryStatus.SUCCESS, db, commit=False)
+                    update_status_by_request_id(request.request_id, DepositRequestStatus.SUCCESS, db, commit=False)
                     db.commit()
                     deposit_status = True
+                    logger.debug(f'Saved a transaction in deposit history DB [user_id: {user_id} -tx_hash: {tx_hash}]')
+
 
                 except Exception as e :
                     logger.error(f'Rollback in saving tx [exception: {e} -data: {data}]')
@@ -263,19 +295,18 @@ class DepositCeleryTaskImpl(DepositCeleryTask):
     @staticmethod
     def check_deposit_requests():
         
-        logging.debug('call check_deposit_requests')
+        logger.debug('call check_deposit_requests')
 
         db = get_db().__next__()
         
-        logger.debug(f'db_deposit_request > get_request_deposit_by_status > request [status: {DepositRequestStatus.PROCESSING}]')
-        ls_requests = get_request_deposit_by_status(DepositRequestStatus.PROCESSING, db)
-        logger.debug(f'db_deposit_request > get_request_deposit_by_status > response [status: {DepositRequestStatus.PROCESSING} -count: {len(ls_requests)}]')
+        ls_requests = get_request_deposit_by_status(DepositRequestStatus.WAITING, db)
+        logger.debug(f'db_deposit_request > get_request_deposit_by_status > response [status: {DepositRequestStatus.WAITING} -count: {len(ls_requests)}]')
         
         try:
             for request in ls_requests:
 
                 if datetime.now() - request.timestamp > timedelta(hours=1):
-                    update_status_by_request_id(request.request_id, DepositHistoryStatus.FAILED, db, commit=False)
+                    update_status_by_request_id(request.request_id, DepositRequestStatus.EXPIRE, db, commit=False)
             
             db.commit()
             logger.debug(f'check_deposit_requests is Done!')
