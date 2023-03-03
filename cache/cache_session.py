c4a90eaabbe9b62506461a595ae0b86c941fc8d6
amir-wyvern <amirhosein_wyvern@yahoo.com>
2023-03-03 23:21:27 +0330

    add tx_hash safely

--- cache/cache_session.py
+++ cache/cache_session.py
@@ -10,8 +10,17 @@ def get_auth_code_by_session(token: str, db: redis.Redis):
 def set_lock_for_user(user_id, db):
     return db.set(f'lock:user:{user_id}', 'True', ex= 300)
 
+def set_lock_for_tx_hash(tx_hash, db):
+    return db.set(f'lock:tx_hash:{tx_hash}', 'True', ex= 300)
+
 def unlock_user(user_id, db):
     return db.delete(f'lock:user:{user_id}')
 
+def unlock_tx_hash(tx_hash, db):
+    return db.delete(f'lock:tx_hash:{tx_hash}')
+
 def get_status_lock_from_user(user_id, db: redis.Redis):
     return db.get(f'lock:user:{user_id}')
+
+def get_status_lock_from_tx_hash(tx_hash, db: redis.Redis):
+    return db.get(f'lock:tx_hash:{tx_hash}')
