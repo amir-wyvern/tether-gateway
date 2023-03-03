80bc1a49a89a4009a223b29fdd757fe6e2e2ba78
amir-wyvern <amirhosein_wyvern@yahoo.com>
2023-03-03 23:36:09 +0330

    add logger and create request_id and enbale history route

--- router/withdraw.py
+++ router/withdraw.py
@@ -1,7 +1,8 @@
 from fastapi.responses import JSONResponse
 from fastapi import (
     APIRouter,
-    Depends
+    Depends,
+    HTTPException
 )
 from schemas import (
     HTTPError,
@@ -14,7 +15,7 @@ from schemas import (
 
 from uuid import uuid4
 from db.database import get_db
-from db import db_withdraw_history, db_main_account, db_config
+from db import db_withdraw, db_main_account, db_config
 
 from celery_tasks.tasks import WithdrawCeleryTask 
 from celery_tasks.utils import create_worker_from
@@ -23,6 +24,25 @@ from datetime import datetime
 from auth.oauth2 import ( 
     get_current_user
 )
+from db.db_user import get_user
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+# Create a console handler to show logs on terminal
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s | %(message)s')
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+
+# Create a file handler to save logs to a file
+file_handler = logging.FileHandler('withdraw_route.log')
+file_handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s | %(message)s')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 router = APIRouter(prefix='/withdraw', tags=['Withdraw'])
 
@@ -33,8 +53,24 @@ def withdraw_request(request: WithdrawRequest, user_id: int=Depends(get_current_
 
     request_id = uuid4().hex[:12]
 
-    withdraw_address = db_main_account.get_withdraw_address(db)
     config = db_config.get_config(db)
+
+    if config.withdraw_lock == True:
+        logger.info(f'withdraw has locked [request_id: {request_id}]')
+        raise HTTPException(status_code=403, detail={'internal_code':1018, 'message':'withdraw has locked'})
+
+    user_data = get_user(user_id, db)
+    
+    balance = float(user_data.balance)
+
+    withdraw_fee = float(config.withdraw_fee_percentage) / 100 * request.value 
+    min_user_balance = float(config.min_user_balance)
+
+    if ( min_user_balance + withdraw_fee + request.value ) > balance :
+        logger.info(f'The user does not have enough balance to withdraw the desired amount ( config.min_user_balance + withdraw_fee + value ) > balance [request_id: {request_id}]')
+        raise HTTPException(status_code=403, detail={'internal_code':1011, 'message':'Insufficient inventory'})
+
+    withdraw_address = db_main_account.get_withdraw_address(db)
 
     request_data = {
         'request_id': request_id,
@@ -51,7 +87,7 @@ def withdraw_request(request: WithdrawRequest, user_id: int=Depends(get_current_
         'processingـcompletionـtime': None
     }
 
-    resp = db_withdraw_history.create_withdraw_history(WithdrawHistoryModelForDataBase(**request_data), db)
+    resp = db_withdraw.create_withdraw_history(WithdrawHistoryModelForDataBase(**request_data), db)
 
     payload = {
         'request_id': request_id,
@@ -68,6 +104,6 @@ def withdraw_request(request: WithdrawRequest, user_id: int=Depends(get_current_
 @router.get('/history', response_model=WithdrawtHistoryResponse, responses={404:{'model':HTTPError}})
 def withdraw_comfirmation(start_time: datetime, end_time: datetime, user_id: int=Depends(get_current_user), db: Session=Depends(get_db)):
 
-    history = db_withdraw_history.get_deposit_history_by_time(user_id, start_time, end_time, db)
+    history = db_withdraw.get_withdraw_history_by_time_and_user(user_id, start_time, end_time, db)
     return JSONResponse(status_code=200, content={'txs': history })
 
