7346a0db87620c2eca614d6397948ac1db1916f7
amir-wyvern <amirhosein_wyvern@yahoo.com>
2023-03-03 23:28:57 +0330

    delete update_config function

--- db/db_main_account.py
+++ db/db_main_account.py
@@ -18,19 +18,3 @@ def get_p_withdraw(db:Session):
 
     resp = db.query(DbMainAccounts).filter(DbMainAccounts.index == 1).first()
     return resp.p_withdraw if resp else None
-
-
-
-# def update_config(request:UpdateConfig , db:Session):
-
-#     user = db.query(DbConfig).filter(DbConfig.index == 1)
-#     user.update({
-#         DbConfig.name: request.name,
-#         DbConfig.lastname: request.lastname,
-#         DbConfig.photo: request.photo,
-#         DbConfig.phone_number: request.phone_number,
-#         DbConfig.email: request.email,
-#     })
-#     db.commit()
-
-#     return True
