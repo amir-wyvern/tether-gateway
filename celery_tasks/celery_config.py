bf3c66a87edea1de9a5c274b8112b1ed73c0e9d5
amir-wyvern <amirhosein_wyvern@yahoo.com>
2023-03-09 17:54:53 +0330

    use redis broker instead of rabbitmq

--- celery_tasks/celery_config.py
+++ celery_tasks/celery_config.py
@@ -1,5 +1,5 @@
-from celery_tasks.rabbitmq_config import RABBITMQ_USER, RABBITMQ_PWD, RABBITMQ_HOST, RABBITMQ_PORT
+# from celery_tasks.rabbitmq_config import RABBITMQ_USER, RABBITMQ_PWD, RABBITMQ_HOST, RABBITMQ_PORT
+from celery_tasks.redis_config import REDIS_DATABASE, REDIS_HOST, REDIS_PORT
+broker_url = f'redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_DATABASE}'
 
-broker_url = f'pyamqp://{RABBITMQ_USER}:{RABBITMQ_PWD}@{RABBITMQ_HOST}:{RABBITMQ_PORT}'
-
-result_backend = 'rpc://'
+result_backend = f'redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_DATABASE}'
