8cd4fd53ad077bc3d8ddc4f3b31e2ff9be7f31e0
amir-wyvern <amirhosein_wyvern@yahoo.com>
2023-03-15 18:01:45 +0330

    add init table

--- db/db_main_account.py
+++ db/db_main_account.py
@@ -1,7 +1,21 @@
 from sqlalchemy.orm.session import Session
 from db.models import DbMainAccounts
+from schemas import InitMainAccount
 
+def init_table(request: InitMainAccount, db:Session):
 
+    data = get_deposit_address(db)
+    if data is None:
+        main_account = DbMainAccounts(
+            deposit_address= request.deposit_address,
+            withdraw_address= request.withdraw_address,
+            p_withdraw= request.p_withdraw
+        )
+        db.add(main_account)
+        db.commit()
+        db.refresh(main_account)
+
+        return main_account
 
 def get_deposit_address(db:Session):
 
