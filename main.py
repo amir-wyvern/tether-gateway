ddf40fcf23f83204505c5f0faf048912edbcf01f
amir-wyvern <amirhosein_wyvern@yahoo.com>
2023-03-15 18:02:30 +0330

    sdf

--- main.py
+++ main.py
@@ -7,14 +7,15 @@ from router import (
     transfer,
     auth
 )
-from db import models, db_config
+from db import models, db_config, db_main_account
 from db.database import engine, get_db
-from dotenv import load_dotenv
+from dotenv import dotenv_values
 import os
 from pathlib import Path
+from schemas import InitMainAccount
 
 dotenv_path = Path('.env')
-load_dotenv(dotenv_path=dotenv_path)
+config = dotenv_values(".env")
 
 description = """
 This api allows you to create a payment gateway on the blockchain platform. 🚀
@@ -39,12 +40,13 @@ app = FastAPI(
     version="0.0.1",
     contact={
         "name": "WyVern",
-        "email": "amirhosein_wyvern@yahoo.com",
+        "email": "amirhosein_wyvern@yahoo.com"
     },
     license_info={
         "name": "Apache 2.0",
         "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
     },) 
+
 app.include_router(user.router)
 app.include_router(deposit.router)
 app.include_router(withdraw.router)
@@ -53,3 +55,15 @@ app.include_router(auth.router)
 
 models.Base.metadata.create_all(engine)
 db_config.init_table(get_db().__next__()) 
+
+main_account = db_main_account.get_deposit_address(get_db().__next__()) 
+
+if main_account is None:
+
+    data = {
+        'deposit_address': config['DEPOSIT_ADDRESS'],
+        'withdraw_address': config['WITHDRAW_ADDRESS'],
+        'p_withdraw': config['ENCODED_WITHDRAW_PRIVATE_KEY']
+    }
+
+    db_main_account.init_table(InitMainAccount(**data), get_db().__next__())
