bc4116c12c842814136fdf8e9e8843f8ef7a6da5
amir-wyvern <amirhosein_wyvern@yahoo.com>
2023-03-22 22:02:14 +0330

    add os.environ.get for load env variables

--- celery_tasks/celery_config.py
+++ celery_tasks/celery_config.py
@@ -1,5 +1,16 @@
 # from celery_tasks.rabbitmq_config import RABBITMQ_USER, RABBITMQ_PWD, RABBITMQ_HOST, RABBITMQ_PORT
 from celery_tasks.redis_config import REDIS_DATABASE, REDIS_HOST, REDIS_PORT
+import os
+
+if os.environ.get('REDIS_HOST'):
+    REDIS_HOST = os.environ.get('REDIS_HOST')
+
+if os.environ.get('REDIS_PORT'):
+    REDIS_PORT = os.environ.get('REDIS_PORT')
+
+if os.environ.get('REDIS_DATABASE'):
+    REDIS_DATABASE = os.environ.get('REDIS_DATABASE')
+
 broker_url = f'redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_DATABASE}'
 
 result_backend = f'redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_DATABASE}'
